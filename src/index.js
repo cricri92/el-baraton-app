4a4b059dd0b145a43947886d6310fad3d6376f0c
Oriana <oriru92@gmail.com>
2019-10-07 22:09:33 -0300

    Adjust index.js to add redux provider.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {devToolsEnhancer} from "redux-devtools-extension";
+
 import App from './App';
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from "store/reducers";
+
+let store = createStore(rootReducer, devToolsEnhancer({}));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
