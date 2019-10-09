9691b05bc60abd2cab5ff8415060dd8471c76b7c
Oriana <oriru92@gmail.com>
2019-10-09 20:07:48 -0300

    Create basic structure for home page.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import MainTitle from "./components/MainTitle";
+import ProductsList from "screens/Products/components/ProductsList";
+import ProductsService from "services/products";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <MainTitle titleText={"Nuestra Selección"}/>
-    </div>
-  )
-};
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productsList: []
+    }
+  }
+
+  componentDidMount() {
+    new ProductsService().fetchAllProducts()
+      .then(productsList => {
+        this.setState({
+          productsList
+        });
+      });
+  }
+
+  render() {
+    const { productsList } = this.state;
+
+    return (
+      <div className="container-fluid">
+        <MainTitle className="pt-5 pb-3 pb-lg-5" titleText={"Las ofertas del día"}/>
+        <ProductsList productsList={productsList} />
+      </div>
+    )
+  }
+}
 
 export default Home;
