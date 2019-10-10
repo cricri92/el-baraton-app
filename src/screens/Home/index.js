cd9291023e2a9a74c7c9f4019cd1c9513cf19844
Oriana Ruiz <oriana.ruiz@4rsoluciones.com>
2019-10-09 21:41:21 -0300

    Add basic carousel for home view.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
+import {Carousel, CarouselItem} from "reactstrap";
 
 import MainTitle from "./components/MainTitle";
 import ProductsList from "screens/Products/components/ProductsList";
 import ProductsService from "services/products";
+
+import freshVariety from 'assets/img/fresh-variety.png';
+import baratoVivir from 'assets/img/barato-vivir.png';
+
+import './styles.scss';
 
 class Home extends Component {
   constructor() {
     super();
 
     this.state = {
-      productsList: []
-    }
+      productsList: [],
+      activeIndex: 0
+    };
+
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
   }
 
   componentDidMount() {
@@ -22,12 +32,34 @@ class Home extends Component {
       });
   }
 
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === [1,2].length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? [1,2].length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
   render() {
-    const { productsList } = this.state;
+    const { productsList, activeIndex } = this.state;
 
     return (
-      <div className="container-fluid">
-        <MainTitle className="pt-5 pb-3 pb-lg-5" titleText={"Las ofertas del día"}/>
+      <div>
+        <MainTitle className="pt-5 pb-3 pb-lg-5" titleText={"Ofertas imperdibles"}/>
+        <Carousel activeIndex={activeIndex}
+                  previous={this.previous}
+                  next={this.next}>
+          <CarouselItem>
+            <img className="home-carousel-img" src={freshVariety} alt="Frescura y variedad"/>
+          </CarouselItem>
+          <CarouselItem>
+            <img className="home-carousel-img" src={baratoVivir} alt="Barato vivir Café"/>
+          </CarouselItem>
+        </Carousel>
         <ProductsList productsList={productsList} />
       </div>
     )
