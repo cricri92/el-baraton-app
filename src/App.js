323811c159c62c83b5c25504968d48d0b89e338a
Oriana Ruiz <oriana.ruiz@4rsoluciones.com>
2019-10-12 23:52:00 -0300

    Add use of footer component.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomeDetail from "screens/HomeDetail";
 import Header from "components/Header";
 
 import 'App.scss';
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Header/>
             <Route exact path="/" component={Home} />
             <Route path="/home-detail" component={HomeDetail} />
+            <Footer />
           </div>
         </Router>
     )
