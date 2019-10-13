6614245e859b7e702c15a4a1de4dd9d4e9ca7306
Oriana Ruiz <oriana.ruiz@4rsoluciones.com>
2019-10-12 23:53:55 -0300

    Add method to query best product offer.

--- src/services/products.js
+++ src/services/products.js
@@ -1,8 +1,28 @@
-import products from 'datasource/products';
+import { products } from 'datasource/products';
 
 class ProductsService {
+    /**
+     * Returns all registered products
+     * @returns {Promise<array>}
+     */
     fetchAllProducts() {
-        return new Promise((resolve, reject) => resolve(products.products));
+        return new Promise((resolve, reject) => resolve(products));
+    }
+
+    /**
+     * Returns most affordable products
+     * @param top
+     * @returns {Promise<array>}
+     */
+    fetchMostAffordableProducts(top = 3) {
+        return new Promise((resolve, reject) =>
+          resolve(
+            products.sort(
+              (a, b) =>
+                parseFloat(a.price.replace(/[$,]/g, '')) - parseFloat(b.price.replace(/[$,]/g, ''))
+            ).slice(0, top)
+          )
+        )
     }
 }
 
