3c49704ac22e568ae5a1842bd29aff329a18de18
Oriana Ruiz <oriana.ruiz@4rsoluciones.com>
2019-10-12 23:51:49 -0300

    Adjust anchor tags source urls.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,39 +15,39 @@ const Footer = () => {
           <div className="col-6 col-md">
             <h5>Features</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Cool stuff</a></li>
-              <li><a className="text-muted" href="#">Random feature</a></li>
-              <li><a className="text-muted" href="#">Team feature</a></li>
-              <li><a className="text-muted" href="#">Stuff for developers</a></li>
-              <li><a className="text-muted" href="#">Another one</a></li>
-              <li><a className="text-muted" href="#">Last time</a></li>
+              <li><a className="text-muted" href="http://google.com">Cool stuff</a></li>
+              <li><a className="text-muted" href="http://google.com">Random feature</a></li>
+              <li><a className="text-muted" href="http://google.com">Team feature</a></li>
+              <li><a className="text-muted" href="http://google.com">Stuff for developers</a></li>
+              <li><a className="text-muted" href="http://google.com">Another one</a></li>
+              <li><a className="text-muted" href="http://google.com">Last time</a></li>
             </ul>
           </div>
           <div className="col-6 col-md">
             <h5>Resources</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Resource</a></li>
-              <li><a className="text-muted" href="#">Resource name</a></li>
-              <li><a className="text-muted" href="#">Another resource</a></li>
-              <li><a className="text-muted" href="#">Final resource</a></li>
+              <li><a className="text-muted" href="http://google.com">Resource</a></li>
+              <li><a className="text-muted" href="http://google.com">Resource name</a></li>
+              <li><a className="text-muted" href="http://google.com">Another resource</a></li>
+              <li><a className="text-muted" href="http://google.com">Final resource</a></li>
             </ul>
           </div>
           <div className="col-6 col-md">
             <h5>Resources</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Business</a></li>
-              <li><a className="text-muted" href="#">Education</a></li>
-              <li><a className="text-muted" href="#">Government</a></li>
-              <li><a className="text-muted" href="#">Gaming</a></li>
+              <li><a className="text-muted" href="http://google.com">Business</a></li>
+              <li><a className="text-muted" href="http://google.com">Education</a></li>
+              <li><a className="text-muted" href="http://google.com">Government</a></li>
+              <li><a className="text-muted" href="http://google.com">Gaming</a></li>
             </ul>
           </div>
           <div className="col-6 col-md">
             <h5>About</h5>
             <ul className="list-unstyled text-small">
-              <li><a className="text-muted" href="#">Team</a></li>
-              <li><a className="text-muted" href="#">Locations</a></li>
-              <li><a className="text-muted" href="#">Privacy</a></li>
-              <li><a className="text-muted" href="#">Terms</a></li>
+              <li><a className="text-muted" href="http://google.com">Team</a></li>
+              <li><a className="text-muted" href="http://google.com">Locations</a></li>
+              <li><a className="text-muted" href="http://google.com">Privacy</a></li>
+              <li><a className="text-muted" href="http://google.com">Terms</a></li>
             </ul>
           </div>
         </div>
