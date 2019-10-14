9408036b44e1826e9c3abb55786caacb562a0923
Oriana Ruiz <oriru92@gmail.com>
2019-10-14 01:03:10 -0300

    Add props extension to add button component. Fix styles for app button component. Add style type button.

--- src/components/AppButton/index.js
+++ src/components/AppButton/index.js
@@ -2,21 +2,28 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
+import {PRIMARY_BUTTON, SECONDARY_BUTTON} from "components/AppButton/constants";
+
 import './styles.scss';
 
-const AppButton = ({ children, classNames }) => {
+const AppButton = ({ children, classNames, actionButton, type, ...props }) => {
   return (
     <button className={classnames({
-      "app-button": true,
-      [classNames]: classNames !== undefined
-    })}>
-      {children}
+        "app-button": true,
+        "app-button--secondary": type === SECONDARY_BUTTON,
+        [classNames]: classNames !== undefined
+      })}
+      onClick={actionButton}
+      {...props}>
+        {children}
     </button>
   );
 };
 
 AppButton.propTypes = {
   children: PropTypes.node.isRequired,
+  actionButton: PropTypes.func.isRequired,
+  type: PropTypes.oneOf([PRIMARY_BUTTON, SECONDARY_BUTTON]),
   classNames: PropTypes.string
 };
 
