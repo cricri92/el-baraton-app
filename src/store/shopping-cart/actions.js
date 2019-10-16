1b0b2b05279689f1314399b2e8f2f39f804a41bd
Oriana <oriru92@gmail.com>
2019-10-16 20:12:46 -0300

    Add actions for cart checkout feature.

--- src/store/shopping-cart/actions.js
+++ src/store/shopping-cart/actions.js
@@ -1,9 +1,30 @@
-import {ADD_TO_SHOPPING_CART, SET_SELECTED_PRODUCT, UNSET_SELECTED_PRODUCT} from "./types";
+import {
+  ADD_TO_SHOPPING_CART, DECREMENT_PRODUCT_QTY,
+  INCREMENT_PRODUCT_QTY,
+  REMOVE_FROM_SHOPPING_CART,
+  SET_SELECTED_PRODUCT,
+  UNSET_SELECTED_PRODUCT
+} from "./types";
 
 export const addToShoppingCart = (product, quantity) => ({
   type: ADD_TO_SHOPPING_CART,
   product,
   quantity
+});
+
+export const removeFromShoppingCart = productId => ({
+  type: REMOVE_FROM_SHOPPING_CART,
+  productId
+});
+
+export const incrementProductQty = product => ({
+  type: INCREMENT_PRODUCT_QTY,
+  product
+});
+
+export const decrementProductQty = product => ({
+  type: DECREMENT_PRODUCT_QTY,
+  product
 });
 
 export const setSelectedProduct = product => ({
--- src/store/shopping-cart/reducer.js
+++ src/store/shopping-cart/reducer.js
@@ -1,4 +1,9 @@
-import {ADD_TO_SHOPPING_CART, SET_SELECTED_PRODUCT, UNSET_SELECTED_PRODUCT} from "store/shopping-cart/types";
+import {
+    ADD_TO_SHOPPING_CART, DECREMENT_PRODUCT_QTY, INCREMENT_PRODUCT_QTY,
+    REMOVE_FROM_SHOPPING_CART,
+    SET_SELECTED_PRODUCT,
+    UNSET_SELECTED_PRODUCT
+} from "store/shopping-cart/types";
 
 const initialState = {
     productList: [],
@@ -9,18 +14,48 @@ const initialState = {
 const shoppingCart = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TO_SHOPPING_CART:
-            const existInCart = state.productList.find(product => product.product.id === action.product.id);
+            const existInCart = state.productList.find(({product}) => product.id === action.product.id);
 
             return {
                 ...state,
                 productList: [
-                  ...state.productList.filter(product => product.product.id !== action.product.id),
+                  ...state.productList.filter(({product}) => product.id !== action.product.id),
                   {
                       product: action.product,
                       quantity: existInCart ? existInCart.quantity + action.quantity : action.quantity
                   }
                 ],
                 totalCartProducts: state.totalCartProducts + action.quantity
+            };
+        case REMOVE_FROM_SHOPPING_CART:
+            return {
+                ...state,
+                productList: state.productList.filter(({product}) => product.id !== action.productId)
+            };
+        case INCREMENT_PRODUCT_QTY:
+            const product = state.productList.find(({product}) => product.id === action.product.product.id);
+            console.log(product);
+
+            return {
+                ...state,
+                productList: [
+                     ...state.productList.filter(({product}) => product.id !== action.product.id),
+                    {
+                        ...product,
+                        quantity: product.quantity + 1
+                    }
+                ]
+            };
+        case DECREMENT_PRODUCT_QTY:
+            return {
+                ...state,
+                productList: [
+                    ...state.productList,
+                    {
+                        ...action.product,
+                        quantity: action.product.quantity > 1 ? action.product.quantity - 1 : 1
+                    }
+                ]
             };
         case SET_SELECTED_PRODUCT:
             return {
