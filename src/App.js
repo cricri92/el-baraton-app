1a0c521f04e7aa22751b761ad50d6e330a7ba2c4
Oriana <oriru92@gmail.com>
2019-10-16 20:09:46 -0300

    Add link to checkout route.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "screens/Home";
-import HomeDetail from "screens/HomeDetail";
+import Checkout from "screens/Checkout";
 import Header from "components/Header";
 
 import 'App.scss';
@@ -14,7 +14,7 @@ const App = () => {
           <div className="app">
             <Header/>
             <Route exact path="/" component={Home} />
-            <Route path="/checkout" component={HomeDetail} />
+            <Route path="/checkout" component={Checkout} />
             <Footer />
           </div>
         </Router>
